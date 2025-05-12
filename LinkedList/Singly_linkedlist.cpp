afb1d7b246c8cbd478696fe1233b1102736b9149
Debjit Saha <debjitsaha606@gmail.com>
2025-05-12 19:57:21 +0530

    insert at head functio added

---
+++ LinkedList/Singly_linkedlist.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+
+    public:
+        int val;
+        Node* next;
+
+        Node(int data){
+            val = data;
+            next = nullptr;
+        }
+};
+
+void insertAtHead(Node* &head,int val){
+    Node* new_node = new Node(val);
+    new_node->next = head;
+    head = new_node;
+}
+
+void display(Node* head){
+
+    Node* temp = head;
+
+    while(temp!=nullptr){
+        cout<<temp->val<<"->";
+        temp=temp->next;
+    }
+    cout<<"X"<<endl;
+    
+}
+
+int main(){
+    Node* head = nullptr;
+    insertAtHead(head,2);
+    insertAtHead(head,3);
+    insertAtHead(head,4);
+    insertAtHead(head,5);
+    
+    display(head);
+
+    //cout<<n->val<<" "<<n->next;
+}
