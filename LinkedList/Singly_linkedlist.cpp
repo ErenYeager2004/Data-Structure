485729534e920e5c09fd4efa5d5ba54a89851f9d
Debjit Saha <debjitsaha606@gmail.com>
2025-05-13 09:48:32 +0530

    insert at any pos and update at any pos func added

--- LinkedList/Singly_linkedlist.cpp
+++ LinkedList/Singly_linkedlist.cpp
@@ -36,9 +36,40 @@ void insertAtTail(Node* &head,int val){
     Node* temp = head;
     while(temp->next!=nullptr){
         temp=temp->next;
-
     }
     temp->next=new_node;
+}
+
+void insertAtAnypos(Node* &head,int val,int pos){
+
+    Node* temp = head;
+    if(pos == 0){
+        insertAtHead(head,val);
+        return;
+    }
+    int curpos = 0;
+    while (curpos!=pos-1)
+    {
+        temp=temp->next;
+        curpos++;
+    }
+
+    Node* new_node = new Node(val);
+    new_node->next=temp->next;
+    temp->next=new_node;
+
+}
+
+void updateAtAnypos(Node* head,int val,int pos){
+    Node* temp = head;
+    int curpos=0;
+    while (curpos!=pos){
+        temp=temp->next;
+        curpos++;
+    }
+    
+    temp->val = val;
+    
 }
 
 int main(){
@@ -50,6 +81,12 @@ int main(){
     
     display(head);
     insertAtTail(head,10);
+    display(head);
+
+    insertAtAnypos(head,34,2);
+    display(head);
+
+    updateAtAnypos(head,33,2);
     display(head);
 
     //cout<<n->val<<" "<<n->next;
