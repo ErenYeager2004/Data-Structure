e894c12800bc28bc88c498e0cc672c2dc1ba2392
Debjit Saha <debjitsaha606@gmail.com>
2025-06-29 09:59:18 +0530

    queue implement using STL

---
+++ Queue/QueueImplementationUsingSTL.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+    queue<int>Q;
+    Q.push(10);
+    Q.push(20);
+    Q.push(30);
+    Q.push(40);
+    Q.push(50);
+
+    Q.pop();
+
+    while(!Q.empty()){
+        cout<<Q.front()<< " ";
+        Q.pop();
+    }
+}
